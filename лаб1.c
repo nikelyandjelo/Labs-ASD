ccd7e2c86efd89200e82f28952141bcf868d4d77
nikelyandjelo <71755968+nikelyandjelo@users.noreply.github.com>
2021-01-02 22:20:29 +0200

    Create лаб1.c

---
+++ лаб1.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+void way2(float x);
+void main()
+{
+	float x;
+	printf("Enter x: ");
+	scanf_s("%f", &x);
+	way2(x);
+}
+void way2(float x)
+{
+	float y;
+	if ((x > -15) && (x <= 3))
+	{
+		y = 4 * x * x + 2;
+		printf("Answer : %f\n", y);
+	}
+	else if ((x <= -30) || (x > 20))
+	{
+		y = 3 * x * x * x / 4 - 5;
+		printf("Answer : %f\n", y);
+	}
+	else
+	{
+		printf("no results\n");
+	}
+}
